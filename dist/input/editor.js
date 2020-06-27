69dc50ae935d6bd17bf82c96b2556f95f04e62eb
Zach Harris <zachbharris94@gmail.com>
2020-06-27 16:10:25 -0500

    prep for marketplace

--- dist/input/editor.js
+++ dist/input/editor.js
@@ -6,4 +6,4 @@ export const components = {
   ProgressBar
 }
 
-export const config = {"displayName":"Progress Bar","name":"progress-bar","version":"dev","components":[{"name":"ProgressBar","displayName":"Progress Bar","defaultWidth":375,"defaultHeight":4,"props":[{"name":"currentValue","displayName":"Progress Value","type":"number","default":"10"},{"name":"maxValue","displayName":"Maximum Value","type":"number","default":"100"},{"name":"color","displayName":"Color","type":"color","default":"#00A898"},{"name":"backgroundColor","displayName":"Background Color","type":"color","default":"#00A89840"}],"resizeY":true}]}
+export const config = {"displayName":"Progress Bar","name":"progress-bar","version":"1.0.1","components":[{"name":"ProgressBar","displayName":"Progress Bar","defaultWidth":375,"defaultHeight":4,"props":[{"name":"currentValue","displayName":"Progress Value","type":"number","default":"10"},{"name":"maxValue","displayName":"Maximum Value","type":"number","default":"100"},{"name":"color","displayName":"Color","type":"color","default":"#00A898"},{"name":"backgroundColor","displayName":"Background Color","type":"color","default":"#00A89840"}],"resizeY":true}]}
