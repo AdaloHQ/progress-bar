c27cbb3a00a28dd4f3b4ba103924feaf3958e327
Jeremy Blalock <jeremyjblalock@gmail.com>
2020-08-20 08:30:56 -0500

    Cleaning things up a little

--- dist/input/editor.js
+++
@@ -1,9 +0,0 @@
-import {
-  ProgressBar
-} from '../../index.js'
-
-export const components = {
-  ProgressBar
-}
-
-export const config = {"displayName":"Progress Bar","name":"progress-bar","version":"1.0.1","components":[{"name":"ProgressBar","displayName":"Progress Bar","defaultWidth":375,"defaultHeight":4,"props":[{"name":"currentValue","displayName":"Progress Value","type":"number","default":"10"},{"name":"maxValue","displayName":"Maximum Value","type":"number","default":"100"},{"name":"color","displayName":"Color","type":"color","default":"#00A898"},{"name":"backgroundColor","displayName":"Background Color","type":"color","default":"#00A89840"}],"resizeY":true}]}
